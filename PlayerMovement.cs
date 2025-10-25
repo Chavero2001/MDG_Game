df34b74700ae2a53b984c3d7ff09056e786d7011
Adrien <adrientremblay@Adriens-MacBook-Pro.local>
2025-10-24 21:56:08 -0400

    modify player controller

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Settings")]    
     public float moveSpeed;
     public Transform orientation;
+    public Camera playerCamera;
 
     float horizontalInput;
     float verticalInput;
@@ -13,22 +14,37 @@ public class PlayerMovement : MonoBehaviour
     
     Rigidbody rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Rotation
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        if (groundPlane.Raycast(ray, out float distance)) {
+            Vector3 hitPoint = ray.GetPoint(distance);
+
+            // Look direction (ignore height)
+            Vector3 lookDir = hitPoint - rb.position;
+            lookDir.y = 0;
+
+            if (lookDir.sqrMagnitude > 0.001f) {
+                Quaternion targetRotation = Quaternion.LookRotation(lookDir);
+                rb.MoveRotation(targetRotation); // smoother than setting rb.rotation directly
+            }
+        }
+
+
+        // Movement
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        Debug.Log(horizontalInput);
-
         Vector3 inputDir = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        Vector3 movementVelocity = inputDir * moveSpeed;
+        Vector3 moveDir = transform.TransformDirection(inputDir);
+        Vector3 movementVelocity = moveDir * moveSpeed;
         rb.linearVelocity = movementVelocity;
     }
 }
