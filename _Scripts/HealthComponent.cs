d96726389d9e7c5cff42cff7e61de995d452e438
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-25 15:28:59 -0400

    Extracted the briefcase to it's own prefab

--- _Scripts/HealthComponent.cs
+++ _Scripts/HealthComponent.cs
@@ -6,15 +6,12 @@ public class HealthComponent : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collide");
        if (collision.gameObject.CompareTag("Projectile"))
         {
             Projectile projectile = collision.gameObject.GetComponent<Projectile>();
-            Debug.Log("Hit");
             if (projectile.Parent != gameObject) {
                 health -= projectile.Damage;
                 if (health <= 0) {
-                    Debug.Log("Die");
                     Destroy(gameObject);
                 }
                 Destroy(collision.gameObject);
