7bc100779312cf936a9573b6ed9ad68f1e7ce391
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2025-10-25 15:42:20 -0400

    123

--- PlayerCamera.cs
+++ PlayerCamera.cs
@@ -10,13 +10,13 @@ public class PlayerCamera : MonoBehaviour
 
     void Update()
     {
-        if (cameraPosition.position.z >= 9 && cameraPosition.position.z <= 140)
+        if (cameraPosition.position.z >= 12 && cameraPosition.position.z <= 130)
         {
             Vector3 target = new Vector3(transform.position.x, transform.position.y, cameraPosition.position.z);
             transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * 5f);
         }
 
-        if (cameraPosition.position.x >= 17 && cameraPosition.position.x <= 133)
+        if (cameraPosition.position.x >= 22 && cameraPosition.position.x <= 125)
         {
             Vector3 target = new Vector3(cameraPosition.position.x, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * 5f);
