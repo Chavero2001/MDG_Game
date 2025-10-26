7d167e7e9336adc352b19f231cc10ab17ae00c1e
Emilio Chavero <116393972+Chavero2001@users.noreply.github.com>
2025-10-25 21:19:58 -0400

    DeathScreen

--- _Scripts/MainMenu.cs
+++ _Scripts/MainMenu.cs
@@ -7,6 +7,14 @@ public class MainMenu : MonoBehaviour
     [Header("UI Panels")]
     public GameObject CreditsPanel;
 
+    Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex);
+    }
+
     public void PlayGame()
     {
         Debug.Log("Moving to next scene");
