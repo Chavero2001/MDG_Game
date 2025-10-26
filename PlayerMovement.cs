dc514f37bfce02e3aa98b87a73f5b079fdc79abb
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2025-10-25 20:54:45 -0400

    123

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -8,8 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCamera;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public Vector3 LookDir;
 
+=======
+    public GameObject smoke;
+>>>>>>> Stashed changes
 =======
     public GameObject smoke;
 >>>>>>> Stashed changes
